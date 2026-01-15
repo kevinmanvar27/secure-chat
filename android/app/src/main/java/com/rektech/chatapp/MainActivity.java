3a404accd5bc2ce92224a2f7079a79df9657df15
KrishBusa <krishbusa1300@gmail.com>
2026-01-15 17:56:44 +0530

    random complete

---
+++ android/app/src/main/java/com/rektech/chatapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.rektech.chatapp;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Block screenshots and screen recording
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_SECURE,
+            WindowManager.LayoutParams.FLAG_SECURE
+        );
+    }
+}
